66d5c2e60070cf3bcc09bab55680f3bbfaf3aa31
roma.sergio <roma.sergio@gmail.com>
2017-04-11 14:56:51 +0100

    add reset.scss, .html made changes to the body, main.js ajax been implemented

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
                                              //main.js
+(function($){
+
+$(document).ready(() => {
+
+	var $form = $('#main_form'),
+		$input_search = $form.find('#movie_search');
+
+	$form.on("submit", function(event){ // on submit prevendefault
+
+		//alert($input_search.val());
+
+		$.ajax({
+
+			url : 'http://www.omdbapi.com/?s='+$input_search,
+			type : 'GET',
+			success : function(response){
+
+				console.log(response);
+			}
+
+
+		});
+		
+
+		event.preventDefault(); // prevent the page to go to action page we will use AJAX
+	});	
 
 
 
-                                             
+
+}); // jquery document.ready functions
+
+
+})(jQuery);  // self calling anonymous function 
+
